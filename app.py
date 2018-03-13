22c2f30e794bcf752cc75772315c41baf0205244
WayneJiang <wayne.jiang@bomdic.com>
2018-03-13 17:05:07 +0800

    no message

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 )
 
+import os
+
 app = Flask(__name__)
 
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
