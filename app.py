b6b2405047ba07232fe8a8891411c5527c424f7c
WayneJiang <LibraFaith@gmail.com>
2020-06-04 00:48:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,69 +1,72 @@
+import os
+
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+
 from flask import Flask, request, abort
 
 from linebot import (
-    LineBotApi, WebhookParser, WebhookHandler
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
 
-import os
-
 app = Flask(__name__)
 
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
-
+# Channel Access Token
 line_bot_api = LineBotApi(channel_access_token)
-parser = WebhookParser(channel_secret)
-
-
+# Channel Secret
 handler = WebhookHandler(channel_secret)
+user_id = 'user_id'
 
+# @app.route("/push_function/<string:push_text_str>")
+# def push_message(push_text_str):
+#     line_bot_api.push_message(user_id, TextSendMessage(text=push_text_str))
+#
 
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
-    print('Request body: ' + body)
-
-    # parse webhook body
+    app.logger.info("Request body: " + body)
+    # handle webhook body
     try:
-        events = parser.parse(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-   if event.message.text=="老高":
-              line_bot_api.reply_message(
-                 event.reply_token,
-                 TextSendMessage(text='又在幻想')
-              )
 
+# # 處理訊息
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     message = TextSendMessage(text=event.message.text)
+#     line_bot_api.reply_message(event.reply_token, message)
+
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+
+# 當機器人加入到一個群組，第一次顯示的訊息
 @handler.add(JoinEvent)
 def handle_join(event):
-    welcome_message = "怎樣？可憐那"
+    welcome_message = "大家好！我是Line Bot，請多多指教"  # 可以修改bot進到群組時，出現的字串
     line_bot_api.reply_message(event.reply_token, TextMessage(text=welcome_message))
     print("加入的事件: %s" % JoinEvent)
 
+
 @handler.add(LeaveEvent)
 def handle_leave(event):
-    message = "可憐那"
-    line_bot_api.reply_message(event.reply_token, TextMessage(text=message))
     print("離開 事件: %s" % event)
     print("離開事件的資訊: %s" % event.source)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5555))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
