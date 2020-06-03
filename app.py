1d9f687393c6b23f8471294dc7163b2e686ac678
WayneJiang <LibraFaith@gmail.com>
2020-06-04 00:31:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,9 @@ line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
 
 
+handler = WebhookHandler(channel_secret)
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
@@ -92,6 +95,13 @@ def handle_join(event):
     welcome_message = "怎樣？可憐那"
     line_bot_api.reply_message(event.reply_token, TextMessage(text=welcome_message))
     print("加入的事件: %s" % JoinEvent)
+
+@handler.add(LeaveEvent)
+def handle_leave(event):
+    message = "可憐那"
+    line_bot_api.reply_message(event.reply_token, TextMessage(text=message))
+    print("離開 事件: %s" % event)
+    print("離開事件的資訊: %s" % event.source)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5555))
