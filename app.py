edd6f3ad4ca05b520283abdd89bb68de0968cc3d
WayneJiang <LibraFaith@gmail.com>
2020-06-08 13:39:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -41,12 +41,13 @@ def callback():
         abort(400)
     return 'OK'
 
+keywords = ["又在幻想","再說一次也是一樣"]
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-     if event.message.text == "老高":
-        message = TextSendMessage(text="又在幻想")
+     if "老高" event.message.text:
+        message = TextSendMessage(text=random.choice(keywords))
         line_bot_api.reply_message(event.reply_token, message)
 
 
