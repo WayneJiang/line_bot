2b9003b35668085a02542ab9ee17ae3bad9d412a
WayneJiang <LibraFaith@gmail.com>
2022-03-02 18:19:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 #channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_secret = 'b5ef5faad577943ab91961106aa23cfb'
@@ -45,29 +46,27 @@ def callback():
         abort(400)
     return 'OK'
 
-keywords = ["哪有之前準現在就不準的","我們需要大學以上程度，不是國中生","你們這樣效率很差","乾我屁事"]
+#keywords = ["哪有之前準現在就不準的","我們需要大學以上程度，不是國中生","你們這樣效率很差","乾我屁事"]
 
 # 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-     if "老M掰" in event.message.text:
-        bye_message = "好啊" #離開群組訊息
-        line_bot_api.reply_message(event.reply_token, TextMessage(text=bye_message))
-        line_bot_api.leave_group(event.source.group_id)
-     elif "M" in event.message.text:
-        message = TextSendMessage(text=random.choice(keywords))
-        line_bot_api.reply_message(event.reply_token, message)
-
+#@handler.add(MessageEvent, message=TextMessage)
+#def handle_message(event):
+#     if "老M掰" in event.message.text:
+#        bye_message = "好啊" #離開群組訊息
+#        line_bot_api.reply_message(event.reply_token, TextMessage(text=bye_message))
+#        line_bot_api.leave_group(event.source.group_id)
+#     elif "M" in event.message.text:
+#        message = TextSendMessage(text=random.choice(keywords))
+#        line_bot_api.reply_message(event.reply_token, message)
 
 @app.route('/')
 def index():
-    return 'Hello Bot'
-
+    return 'Wayne\'s LINE Bot'
 
 # 當機器人加入到一個群組，第一次顯示的訊息
 @handler.add(JoinEvent)
 def handle_join(event):
-    welcome_message = "歡迎收聽GoMore M觀點"  #進入群組訊息
+    welcome_message = ""  #進入群組訊息
     line_bot_api.reply_message(event.reply_token, TextMessage(text=welcome_message))
     print("加入的事件: %s" % event)
     print("加入事件的資訊: %s" % event.source)
@@ -77,7 +76,20 @@ def handle_join(event):
 def handle_leave(event):
     print("離開的事件: %s" % event)
     print("離開事件的資訊: %s" % event.source)
+    
+    
+def GetPrice(symbol):
+    try:
+        price = requests.get(https://api.binance.com/api/v3/ticker/price, params={'symbol': symbol}).json()['price']
+    except Exception as e:
+        print ('Error! problem is {}'.format(e.args[0]))
+    return float(price)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    while true:
+        price = GetPrice("BTCUSDT")
+        message = TextSendMessage(text={price})
+        line_bot_api.reply_message(event.reply_token, message)
+        time.sleep(14400)
