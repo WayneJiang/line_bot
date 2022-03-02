18d8046657cf1468f0415385c323e61357486ab4
WayneJiang <LibraFaith@gmail.com>
2022-03-02 18:26:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def GetPrice(symbol):
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-    while true:
+    while True:
         price = GetPrice("BTCUSDT")
         message = TextSendMessage(text={price})
         line_bot_api.reply_message(event.reply_token, message)
