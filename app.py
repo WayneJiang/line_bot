c4dc11bc1c6898c87fff279f8d8ba18d13e86a73
WayneJiang <LibraFaith@gmail.com>
2022-03-02 18:50:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -88,6 +88,8 @@ def GetPrice(symbol):
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    
+    print("X")
     while True:
         print("Query")
         price = GetPrice("BTCUSDT")
